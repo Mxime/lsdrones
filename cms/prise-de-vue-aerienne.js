2a073ba2d9779d7a96fb6d62daf071984182c394
Maxime Martin <maximemartin@MacBook-Pro-de-Maxime.local>
2019-04-07 11:36:13 +0200

    remove nodes_modules from git repo

--- cms/prise-de-vue-aerienne.js
+++ cms/prise-de-vue-aerienne.js
@@ -1,7 +1,10 @@
-{
+({
 	$image_type: 'image',
 	image: '/remote/direct_uploads/1518615695_prise_vue.jpg',
 	$contenu_type: 'quill',
-	contenu: '<h1>Prise de vue aérienne</h1><p><strong>Évènements (fêtes, festivals, mariages…) : valorisation par un point de vue original grâce au drone</strong></p><p>Organisateur de festivals, responsable d’un évènement à fort potentiel médiatique : nous vous proposons des prestations pour valoriser ce dont vous vous occupez en filmant depuis les airs tous les aspects de votre évènement. Notre équipe vous assure également la post-production de vos images afin que vous puissiez poster sur les réseaux sociaux dans les plus brefs délais un montage de qualité professionnelle.</p><h2>Photo, session simple 1 batterie (environ 20 minutes)</h2><p><strong>Tarif : 150 €</strong></p><h2>Vidéo, session simple 1 batterie (environ 20 minutes)</h2><p><strong>Tarif : 250 €</strong></p><h2>Vidéo, heure de vol</h2><p><strong>Tarif : 400 €</strong></p><h2>Post-production vidéo (traitement d’images, montage, titrage, mise en musique)</h2><p><strong>Tarif : 300 €</strong></p>',
-	$contenu_control_path: '/brick/enduro_quill/quill_control'
-}
+	contenu: '<h1>Prise de vue aérienne</h1><p><strong>Évènements (fêtes, festivals, mariages…) : valorisation par un point de vue original grâce au drone</strong></p><p>Organisateur de festivals, responsable d’un évènement à fort potentiel médiatique : nous vous proposons des prestations pour valoriser ce dont vous vous occupez en filmant depuis les airs tous les aspects de votre évènement. Notre équipe vous assure également la post-production de vos images afin que vous puissiez poster sur les réseaux sociaux dans les plus brefs délais un montage de qualité professionnelle.</p><h2>Photo, session simple 1 batterie (environ 20 minutes)</h2><h2>Vidéo, session simple 1 batterie (environ 20 minutes)</h2><h2>Vidéo, heure de vol</h2><h2>Post-production vidéo (traitement d’images, montage, titrage, mise en musique)</h2>',
+	$contenu_control_path: '/brick/enduro_quill/quill_control',
+	meta: {
+		last_edited: 1554629674
+	}
+})
